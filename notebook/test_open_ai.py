328151dfcdd6f7c2c94cb1d2470ac69fc326f99e
hellozhaojian <290169713@qq.com>
2023-07-03 00:23:18 +0800

    update openai key

--- notebook/test_open_ai.py
+++ notebook/test_open_ai.py
@@ -8,9 +8,9 @@ proxies = {
 }
 openai.proxy = proxies
 openai.api_key = "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"
-openai.api_key = "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"
-openai.api_key = "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"
-openai.api_key = "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"
+#openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key = "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"
 model = "gpt-3.5-turbo"
 #model = "gpt-3.5-turbo-0613"
 
